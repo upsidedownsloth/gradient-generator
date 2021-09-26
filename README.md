06e5abf242afb95f22d67a90fd308400acd7239c
upsidedownsloth <skeletonquerida@gmail.com>
2021-09-26 11:13:01 +1000

    add deg input and tidy up README.md

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-// Task: Write a function to:
-// - Generate a gradient using the colors from the pickers and split percentage from the slider. 
-// - Use the gradient as the form background.
+# Gradient Generator
+Scrimba Weekly Web Dev Challenge
+Challenge details: https://scrimba.com/learn/weeklychallenge/the-weekly-web-dev-challenge-gradient-generator-latest-challenge-code-to-win-co54c4b29830fcb5dfe04c953
 
-// Stretch goals:
-// - Add more colors.
-// - Add a 'Surprise Me' button which generates random gradients (and updates the inputs).
-// - Check for contrast between the gradient and the text.
+Live-code: https://www.youtube.com/watch?v=VMldLdMSoBo
+
+## Task: Write a function to:
+- Generate a gradient using the colors from the pickers and split percentage from the slider. 
+- Use the gradient as the form background.
+
+Stretch goals:
+- Add more colors.
+- Add a 'Surprise Me' button which generates random gradients (and updates the inputs).
+- Check for contrast between the gradient and the text. (not done)
--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// Task: Write a function to:
-// - Generate a gradient using the colors from the pickers and split percentage from the slider. 
-// - Use the gradient as the form background.
-
-// Stretch goals:
-// - Add more colors.
-// - Add a 'Surprise Me' button which generates random gradients (and updates the inputs).
-// - Check for contrast between the gradient and the text.
-
-
 const randomBtn = document.getElementById("random-btn")
 const picker1 = document.getElementById("picker-1")
 const picker2 = document.getElementById("picker-2")
 const split = document.getElementById("split")
+const deg = document.getElementById("deg")
 const getGradient = document.getElementById("get-gradient")
 const copyBtn = document.getElementById("copy-gradient")
 const toast = document.getElementById("toast")
@@ -19,6 +10,7 @@ const toast = document.getElementById("toast")
 picker1.addEventListener("input", setGradient)
 picker2.addEventListener("input", setGradient)
 split.addEventListener("input", setGradient)
+deg.addEventListener("input", setGradient)
 randomBtn.addEventListener("click", getRandomGradient)
 copyBtn.addEventListener("click", copyGradient)
 
@@ -27,7 +19,8 @@ function setGradient() {
     const colourOne = picker1.value
     const colourTwo = picker2.value
     const splitValue = split.value
-    getGradient.innerHTML = document.body.style = `background: linear-gradient(${colourOne} ${splitValue}%, ${colourTwo});`
+    const degValue = deg.value
+    getGradient.innerHTML = document.body.style = `background: linear-gradient(${degValue}deg, ${colourOne} ${splitValue}%, ${colourTwo});`
 }
 setGradient()
 
@@ -49,13 +42,6 @@ function showToast(){
     toast.className = "show"
     setTimeout(function(){
         toast.className = toast.className.replace("show", "")
-    }, 3000)
+    }, 1000)
     toast.innerText = "copied!"
 }
-
-
-/* TODO:
-- // add copy code function 
-- add degree for gradient
-- add READMe and oush to remote
-- // add Toast */
